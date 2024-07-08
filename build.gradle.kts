fc1fce75c1b52854c8158ebd666fcc1dcf616a98
bayo-code <hardebahyho@gmail.com>
2024-07-08 19:14:10 +0100

    Add maven publish dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    id("com.vanniktech.maven.publish") version "0.29.0" apply false
 }
 
 buildscript {
